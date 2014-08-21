6619922be81834311a8f089599cd714ac700c6db
Volodymyr-Levchenko <dante70850@gmail.com>
2014-08-21 18:45:58 +0300

    Commit1

---
+++ Auth_training/Auth_training/DAL/AuthContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Auth_training.DAL
+{
+    public class AuthContext:IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+        }
+    }
+}
---
+++ Auth_training/Auth_training/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Auth_training;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
+namespace AngularJSAuthentication.API
+{
+    public class Startup
+    {
+        public void COnfiguration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
